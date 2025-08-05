2fed2b3b42cc0fcd2e4b4df4840e95a37fd9d24f
Md. Nafis Muktashid Arpon <nafismuktashid@gmail.com>
2025-08-05 22:07:16 +0600

    Update SPP-11.asm

--- SPP-11.asm
+++ SPP-11.asm
@@ -1,59 +1,58 @@
 .org 0x100
 
 .data
-    input:    .asciz "madam"     
-    output:   .space 8             
-    yes_str:  .asciz "Yes"
-    no_str:   .asciz "No"
+    input:    .asciz "racecar"     @ Try changing this to "hellyeah"
+    output:   .space 8             @ To store "Yes" or "No"
+    yes:      .asciz "Yes"
+    no:       .asciz "No"
 
 .text
 .global _start
 
 _start:
-    LDR R0, =input         
-    MOV R1, #0             
+    LDR R0, =input         @ R0: base of input
+    MOV R1, #0             @ R1: counter to find string length
 
 find_len:
     LDRB R2, [R0, R1]
     CMP R2, #0
-    BEQ check_palindrome
+    BEQ start_check
     ADD R1, R1, #1
     B find_len
 
-check_palindrome:
-    SUB R1, R1, #1         
-    MOV R2, #0             
-    LDR R3, =input         
+start_check:
+    SUB R1, R1, #1         @ R1 = end index (length - 1)
+    MOV R2, #0             @ R2 = start index
 
-compare_loop:
+check_loop:
     CMP R2, R1
-    BGE is_palindrome
+    BGE is_palindrome      @ If start >= end, it's a palindrome
 
-    LDRB R4, [R3, R2]      
-    LDRB R5, [R3, R1]      
-    CMP R4, R5
+    LDRB R3, [R0, R2]      @ char at start
+    LDRB R4, [R0, R1]      @ char at end
+    CMP R3, R4
     BNE not_palindrome
 
     ADD R2, R2, #1
     SUB R1, R1, #1
-    B compare_loop
+    B check_loop
 
 is_palindrome:
-    LDR R0, =yes_str
+    LDR R5, =yes
     B store_result
 
 not_palindrome:
-    LDR R0, =no_str
+    LDR R5, =no
 
 store_result:
-    LDR R1, =output
+    LDR R6, =output
 
-copy_loop:
-    LDRB R2, [R0], #1
-    STRB R2, [R1], #1
-    CMP R2, #0
-    BNE copy_loop
+copy:
+    LDRB R7, [R5], #1
+    STRB R7, [R6], #1
+    CMP R7, #0
+    BNE copy
 
 exit:
-    MOV R7, #1     
+    MOV R7, #1
     SWI 0
